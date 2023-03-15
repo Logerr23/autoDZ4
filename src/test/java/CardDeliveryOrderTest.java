3975e7958acf28743f55938003a656b1491c626a
Igor D <logerr23@gmail.com>
2023-03-15 15:39:03 +0300

    changes tests

--- src/test/java/CardDeliveryOrderTest.java
+++ src/test/java/CardDeliveryOrderTest.java
@@ -3,66 +3,63 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryOrderTest {
+    public String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
 
 
     @Test
     void shouldOrderCardDelivery(){
         Configuration.holdBrowserOpen = true;
-        Calendar cal = new GregorianCalendar();
-        cal.add(Calendar.DAY_OF_YEAR, 3);
-        SimpleDateFormat sdf = new SimpleDateFormat("dd MM yyyy");
-        String date = sdf.format(cal.getTime());
+        String planningDate = generateDate(3, "dd.MM.yyyy");
 
         open("http://localhost:9999/");
         $x("//*[@data-test-id = 'city']//input").setValue("Санкт-Петербург");
-        $x("//*[@data-test-id = 'date']//input").sendKeys("\b\b\b\b\b\b\b\b");
-        $x("//*[@data-test-id = 'date']//input").setValue(date);
+        $x("//*[@data-test-id = 'date']//input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $x("//*[@data-test-id = 'date']//input").setValue(planningDate);
         $x("//*[@data-test-id = 'name']//input").setValue("Поттер Гарри");
         $x("//*[@data-test-id = 'phone']//input").setValue("+78005553535");
         $x("//*[@data-test-id = 'agreement']").click();
         $x("//*[text() = 'Забронировать']").click();
 
-        $x("//*[@data-test-id = 'notification']").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 
     @Test
     void shouldOrderCardDeliveryUseAutocompleteForms(){
         Configuration.holdBrowserOpen = true;
 
-        Calendar calWithCurrentDate = new GregorianCalendar();      //календарь с текущей датой
-        Calendar calWithSetDate = new GregorianCalendar();          // календарь с выбранной дата
-        calWithSetDate.add(Calendar.DAY_OF_YEAR, 7);
-        int currentMonth = calWithCurrentDate.get(Calendar.MONTH);  // текущий месяц
-        int setMonth = calWithSetDate.get(Calendar.MONTH);          // выбранный месяц
-        int setDay =calWithSetDate.get(Calendar.DAY_OF_MONTH);      //выбранный день
-
+        long setDay = 7;
+        LocalDate currentDate = LocalDate.now();
+        String planningDate = generateDate(setDay, "dd.MM.yyyy");
 
         open("http://localhost:9999/");
         $x("//*[@data-test-id = 'city']//input").setValue("Са");
         $x("//span[text() = 'Санкт-Петербург']").click();
+
         $x("//*[@data-test-id = 'date']//button").click();
-        if(currentMonth < setMonth ){
-            $x("//div[@data-step = '1']").click();           // выбор даты с перелистыванием месяца
-            $x("//td[text() = '" + setDay +"']").click();
-        }else{
-            $x("//td[text() = '" + setDay +"']").click();    // выбор даты без перелистывания месяца
+        for (int i = currentDate.getMonthValue(); i < currentDate.plusDays(setDay).getMonthValue(); i++) {     //сделал более универсальным, теперь вне зависимости
+            $x("//div[@data-step = '1']").click();                                               //сколько прибавится дней, месяц накликается правильно
         }
+        $x("//td[text() = '" + currentDate.plusDays(setDay).getDayOfMonth() +"']").click();
 
         $x("//*[@data-test-id = 'name']//input").setValue("Поттер Гарри");
         $x("//*[@data-test-id = 'phone']//input").setValue("+78005553535");
         $x("//*[@data-test-id = 'agreement']").click();
         $x("//*[text() = 'Забронировать']").click();
 
-        $x("//*[@data-test-id = 'notification']").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 
 
